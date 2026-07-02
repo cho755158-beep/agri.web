5d01ccefa3db10b2dd50ec00ae78b698a7892d91
cho755158-beep <cho755158@gmail.com>
2026-07-02 09:29:32 +0700

    Update configure.swift

--- Agri_Web/Sources/Agri_Web/configure.swift
+++ Agri_Web/Sources/Agri_Web/configure.swift
@@ -3,7 +3,7 @@ import Vapor
 /// configures your application
 func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
-     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory + "index.html"))
+     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
     // register routes
     
